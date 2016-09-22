4fc1777ddfb6f8def87ee280af99d00f4d810b2a
xiaomuxue <1017579156@qq.com>
2016-09-22 14:39:40 +0800

    express case

---
+++ session/server.js
@@ -0,0 +1,33 @@
+/**
+ * Created by YC on 2016/9/21.
+ */
+var express=require("express");
+var bodyparser=require("body-parser");
+var session=require("express-session");
+
+var app=express();
+app.use(express.static(__dirname));
+app.use(bodyparser.urlencoded({extended:false}));
+app.use(session({
+    secret:'keyboard cat',    //私密  session id的标识
+    resave:true,        //每次请求是否重新设置session cookie
+    saveUnitialized:true,   //设置session  cookie，默认值为connect.sid
+    cookie:{secure:false,maxAge:1000*60}    //secure  用于https
+}));
+
+app.post("/userLogin",function(req,res){
+     req.session.uname=req.body.uname;
+     res.send("1");
+});
+
+app.get("/currentUserName",function(req,res){
+    res.send("当前登录用户："+req.session.uname);
+});
+
+app.listen(8888,function(err){
+    if(err){
+        console.info(err);
+    }else{
+        console.info("服务器启动成功......");
+    }
+});
